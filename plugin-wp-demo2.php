b95af2ecf7345625ca01cfa188f481b002418c26
kadjemar <kouchoro@gmail.com>
2025-04-01 13:33:29 +0100

    add main file

---
+++ plugin-wp-demo2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Plugin WP Demo 2
+ * Plugin URI: https://example.com
+ * Description: A simple WordPress plugin demo.
+ * Version: 1.0.0
+ * Author: Your Name
+ * Author URI: https://example.com
+ * License: GPL2
+ */
+
+// 
